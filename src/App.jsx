7ac07bf52caec751354409c8432da5275cc5096d
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2025-12-07 23:18:21 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 class="text-[var(--color-primary)]">sawon</h1>
+      <button className='btn btn-primary '>md al jihad</button>
+    </>
+  )
+}
+
+export default App
